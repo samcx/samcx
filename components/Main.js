c777bde691b7d596d3e4e5d84c1c344a951dc54a
samsisle <samko9522@gmail.com>
2019-05-17 03:06:40 -0700

    🍿 Add Main component

---
+++ components/Main.js
@@ -0,0 +1,11 @@
+import css from '../css/main.css';
+
+export default function Main() {
+  return (
+    <main className={css.main}>
+      <section>
+        <h1 className={css.title}>Hi! I'm Sam. Full-stack web developer.</h1>
+      </section>
+    </main>
+  );
+}
