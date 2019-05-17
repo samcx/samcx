e05546452f29452d38ddd8e8dd022bdc011ade4a
samsisle <samko9522@gmail.com>
2019-05-17 03:06:52 -0700

    🍩 Add Meta component

---
+++ components/Meta.js
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function Meta() {
+  return (
+    <Head>
+      <title>samsisle</title>
+      <link href="/static/index.css" rel="stylesheet" />
+    </Head>
+  );
+}
