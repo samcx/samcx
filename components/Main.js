69c38384dc2f367672e061680c7aec1fa7957c6f
samsisle <samko9522@gmail.com>
2019-05-18 23:47:38 -0700

    🍿 Update Main component

--- components/Main.js
+++ components/Main.js
@@ -4,7 +4,8 @@ export default function Main() {
   return (
     <main className={css.main}>
       <section>
-        <h1 className={css.title}>Hi! I'm Sam. Full-stack web developer.</h1>
+        <h1 className={css.title}>Hi! I'm Sam. Full-stack web developer. Socal native. Beach lover.</h1>
+        <p className={css.description}>The Ohio State University and Lambda School graduate.</p>
       </section>
     </main>
   );
