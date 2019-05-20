ef49d750499647f6ae0922c60254b7420a3fbb45
samsisle <samko9522@gmail.com>
2019-05-20 00:45:16 -0700

    🍩 Update Meta component

--- components/Meta.js
+++ components/Meta.js
@@ -4,7 +4,7 @@ export default function Meta() {
   return (
     <Head>
       <title>samsisle</title>
-      <link href="/static/index.css" rel="stylesheet" />
+      <link href="/static/lib/index.css" rel="stylesheet" />
     </Head>
   );
 }
