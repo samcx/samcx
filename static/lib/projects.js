5a4fec74a1752deefb52168d93d9ba16de32af46
samsisle <samko9522@gmail.com>
2019-05-20 00:42:11 -0700

    🍨 Add projects data (array)

---
+++ static/lib/projects.js
@@ -0,0 +1,25 @@
+export const projects = [
+  {
+    category: 'personal',
+    title: 'Emojiest',
+    description: 'A simple source for all your favorite emojis.',
+    view: 'https://emojiest.netlify.com',
+    source: 'https://github.com/samsisle/emojiest'
+  },
+  {
+    category: 'capstone',
+    title: 'LearnLocker',
+    description:
+      'A web application that allows users to keep, track, and organize internet resources in one place.',
+    view: 'https://learnlocker.dev',
+    source: 'https://github.com/learneda'
+  },
+  {
+    category: 'personal',
+    title: 'Instagram Twin',
+    description:
+      "A Lambda School project that closely mimics Instagram's design.",
+    view: 'https://instagram-twin.firebaseapp.com',
+    source: 'https://github.com/samsisle/instagram-twin'
+  }
+];
