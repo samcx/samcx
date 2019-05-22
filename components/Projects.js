99bd0add03f503d116b50e2e8235e90089bae9aa
samsisle <samko9522@gmail.com>
2019-05-21 18:33:53 -0700

    🍬 Update Projects component (ACSS)

--- components/Projects.js
+++ components/Projects.js
@@ -1,24 +1,22 @@
 import { projects } from '../static/lib/projects';
 
-import css from '../css/projects.css';
-
 export default function Projects() {
   return (
-    <section className={css.section__2}>
-      <h2 className={css.title__2}>Projects</h2>
-      <ul className={css.projects}>
+    <section className="pt4">
+      <h2 className="bb bc20 black60 fs16 fs20-td fw500 pb2">Projects</h2>
+      <ul className="flex-m flex-d flex-row">
         {projects.map(project => (
-          <li className={css.project} key={project.title}>
-            <div className={css.category}>{project.category}</div>
+          <li className="pb2 pt4 w30-d" key={project.title}>
+            <div className="black40 fs14 fw500 category">{project.category}</div>
             <a href={project.view} target="_blank" rel="noopener noreferrer">
-              <h3 className={css.title__3}>{project.title}</h3>
+              <h3 className="fs24 fs36-d fw500 pb1 pt1 hover">{project.title}</h3>
             </a>
-            <p className={css.description}>{project.description}</p>
+            <p className="black60 fs16 fw500 lh pb1 pt1">{project.description}</p>
             <a href={project.view} target="_blank" rel="noopener noreferrer">
-              <span className={css.view}>View</span>
+              <span className="black40 fs14 mr1 hover">View</span>
             </a>
             <a href={project.source} target="_blank" rel="noopener noreferrer">
-              <span className={css.source}>Source</span>
+              <span className="black40 fs14 hover">Source</span>
             </a>
           </li>
         ))}
