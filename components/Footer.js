ef5cd61d11af7eca66e3defa81cd9753dfec7922
samsisle <samko9522@gmail.com>
2019-05-21 18:29:31 -0700

    🍋 Update Footer component (ACSS)

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,17 @@
-import css from '../css/footer.css'
+import { links } from '../static/lib/links';
 
 export default function Header() {
   return (
-    <footer className={css.footer}>
-      <div>this is the footer</div>
+    <footer className="m0a pb4 pt4 w90 w80-d">
+      <ul className="flex-m flex-row">
+        {links.map(link => (
+          <li className="mr2-d black60 fs16 pb1 pt1 hover" key={link.name}>
+            <a href={link.href} target="_blank" rel="noopener noreferrer">
+              {link.name}
+            </a>
+          </li>
+        ))}
+      </ul>
     </footer>
   );
 }
