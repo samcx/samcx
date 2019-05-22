e02ad2d30ff889b8cb994979393ee10e0e051595
samsisle <samko9522@gmail.com>
2019-05-21 18:25:42 -0700

    🌴 Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS({
-  target: 'serverless',
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[local]__[hash:base64:5]'
-  }
-});
+module.exports = withCSS({ target: 'serverless' });
