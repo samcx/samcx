ed4bce359abcd4b5604b5df57901992d5a2cad6f
samsisle <samko9522@gmail.com>
2019-05-21 22:39:31 -0700

    🌈 Add Bar component

---
+++ components/Bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Bar() {
+  return (
+    <div className="bar">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
