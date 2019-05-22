996aefd8d2b031b07c5921924dbb16e6a19b6fd1
samsisle <samko9522@gmail.com>
2019-05-21 18:30:34 -0700

    🍿 Update Main component (ACSS)

--- components/Main.js
+++ components/Main.js
@@ -1,15 +1,13 @@
 import Projects from './Projects';
 
-import css from '../css/main.css';
-
 export default function Main() {
   return (
-    <main className={css.main}>
-      <section className={css.section__1}>
-        <h1 className={css.title__1}>
+    <main className="m0a pb4 w90 w80-d">
+      <section className="pb4">
+        <h1 className="fs24 fs36-t fs48-d fw500 pb32">
           Hi! I'm Sam. Full-stack web developer. Socal native. Beach lover.
         </h1>
-        <p className={css.status}>
+        <p className="black60 fs16 fs20-td fw500 lh">
           The Ohio State University graduate. Student at Lambda School.
         </p>
       </section>
