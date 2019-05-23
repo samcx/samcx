94065b532350b840b77f198e02345b3f6d8c78bf
samsisle <samko9522@gmail.com>
2019-05-23 00:41:33 -0700

    🍩 Update Meta component (meta content & favicon)

--- components/Meta.js
+++ components/Meta.js
@@ -3,7 +3,18 @@ import Head from 'next/head';
 export default function Meta() {
   return (
     <Head>
+      <link rel="shortcut icon" href="/static/lib/favicon.ico" />
       <title>samsisle</title>
+      <meta
+        name="description"
+        content="Full-stack web developer. SoCal native. Beach lover."
+      />
+      <meta property="og:title" content="samsisle" />
+      <meta
+        property="og:description"
+        content="Full-stack web developer. SoCal native. Beach lover."
+      />
+      <meta name="twitter:card" content="summary" />
     </Head>
   );
 }
