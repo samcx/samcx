523d1c8278918855857b23edd331a312271e9cdc
samsisle <samko9522@gmail.com>
2019-05-23 00:34:16 -0700

    🍌 Update Header component (img src)

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="m0a pb4 pt4 w90 w80-d">
-      <img src="/static/svg/2600.svg" className="sun" alt="sun" />
+      <img src="/static/lib/2600.svg" className="sun" alt="sun" />
     </header>
   );
 }
