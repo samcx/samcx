e4be9704be198a3376dad79c563d4c276aa85636
samsisle <samko9522@gmail.com>
2019-05-22 23:38:25 -0700

    🍭 Update index page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Meta from '../components/Meta';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import Footer from '../components/Footer';
-import Bar from '../components/Bar'
+import Bar from '../components/Bar';
 
 import '../css/index.css';
 
