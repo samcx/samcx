739071ac22c6128e5eb2df05a613706b77e1c473
samsisle <samko9522@gmail.com>
2019-05-24 00:31:04 -0700

    🎨 Update Main component (classNames)

--- components/Main.js
+++ components/Main.js
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <main className="m0a pb4 w90 w80-d">
       <section className="pb4">
-        <h1 className="fs24 fs36-t fs48-d fw500 pb32">
+        <h1 className="fs24 fs36-t fs48-d fw500 pb3_2">
           Hi! I'm Sam. Full-stack web developer. SoCal native. Beach lover.
         </h1>
         <p className="black60 fs16 fs20-td fw500 lh">
