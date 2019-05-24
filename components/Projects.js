b6fdbdb089572ec17b32d39b53b6d0bb9259565f
samsisle <samko9522@gmail.com>
2019-05-23 23:25:36 -0700

    🍬 Update Projects component (classNames)

--- components/Projects.js
+++ components/Projects.js
@@ -6,7 +6,7 @@ export default function Projects() {
       <h2 className="bb bc20 black60 fs16 fs20-td fw500 pb2">Projects</h2>
       <ul className="flex-m flex-d flex-row">
         {projects.map(project => (
-          <li className="pb2 pt4 w30-d" key={project.title}>
+          <li className="b-d bb bc10 pb4 pt4 w30-d" key={project.title}>
             <div className="black60 fs14 fw500 category">{project.category}</div>
             <a href={project.view} target="_blank" rel="noopener noreferrer">
               <h3 className="fs24 fs36-d fw500 pb1 pt1 hover">{project.title}</h3>
