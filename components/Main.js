dfe1ed950c62f422074af1fffe2ef233cf32ddad
samsisle <samko9522@gmail.com>
2019-05-26 21:45:26 -0700

    🍿 Update Main component (remove fw500 className)

--- components/Main.js
+++ components/Main.js
@@ -4,10 +4,10 @@ export default function Main() {
   return (
     <main className="m0a pb4 w90 w80-d">
       <section className="pb4">
-        <h1 className="fs24 fs36-t fs48-d fw500 pb3_2 recoleta">
+        <h1 className="fs24 fs36-t fs48-d pb3_2 recoleta">
           Hi! I'm Sam. Full-stack web developer. SoCal native. Beach lover.
         </h1>
-        <p className="black60 fs16 fs20-td fw500 lh">
+        <p className="black60 fs16 fs20-td lh">
           The Ohio State University graduate. Student at Lambda School.
         </p>
       </section>
