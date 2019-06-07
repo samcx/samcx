e14cd95026ca054ad5962af0bcc562666c7c064f
samsisle <samko9522@gmail.com>
2019-06-07 04:15:17 -0700

    🍭 Update index page (add comments)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+/**
+ * ./pages/index.js
+ * 
+ * This is the index page. Next.js + Now will automatically process
+ * and render this page first upon loading https://samsisle.dev on 
+ * the browser.
+ */
+
 import Meta from '../components/Meta';
 import Header from '../components/Header';
 import Main from '../components/Main';
