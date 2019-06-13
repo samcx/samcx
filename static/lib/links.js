a2720ee5c17d31433b9757b5d210e6aa43aec8f7
samsisle <samko9522@gmail.com>
2019-06-13 16:47:15 -0700

    🍺 Minify links.js

--- static/lib/links.js
+++ static/lib/links.js
@@ -1,7 +1,1 @@
-export const links = [
-  { name: 'GitHub', href: 'https://github.com/samsisle' },
-  { name: 'Twitter', href: 'https://twitter.com/samsisle' },
-  { name: 'Letterboxd', href: 'https://letterboxd.com/samsisle/' },
-  { name: 'LinkedIn', href: 'https://www.linkedin.com/in/samsisle/' },
-  { name: 'Email', href: 'mailto:samko9522@gmail.com' }
-];
+export const links=[{name:"GitHub",href:"https://github.com/samsisle"},{name:"Twitter",href:"https://twitter.com/samsisle"},{name:"Letterboxd",href:"https://letterboxd.com/samsisle/"},{name:"LinkedIn",href:"https://www.linkedin.com/in/samsisle/"},{name:"Email",href:"mailto:samko9522@gmail.com"}];
