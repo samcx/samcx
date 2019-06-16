f6451a0d5752d92c6a8e5b76d11664f893380688
samsisle <samko9522@gmail.com>
2019-06-15 23:24:27 -0700

    🍬 Update _document page (comments)

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,10 @@
-// _document is only rendered on the server side and not on the client side
-// Event handlers like onClick can't be added to this file
+/**
+ * ./pages/_document.js
+ * 
+ * _document is only rendered on the server side and not on the client side
+ * Event handlers like onClick can't be added to this file
+ */
 
-// ./pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
