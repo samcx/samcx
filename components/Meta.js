25228529062574ca1b3c1bcbed2a9efad227513b
samsisle <samko9522@gmail.com>
2019-07-01 19:02:21 -0700

    🍩 Update meta content srcs

--- components/Meta.js
+++ components/Meta.js
@@ -3,15 +3,15 @@ import Head from 'next/head';
 export default function Meta() {
   return (
     <Head>
-      <link rel="shortcut icon" href="/static/lib/favicon.ico" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/static/lib/apple-touch-icon.png"
+        href="/static/apple-touch-icon.png"
       />
       <link
         rel="mask-icon"
-        href="/static/lib/safari-pinned-tab.svg"
+        href="/static/safari-pinned-tab.svg"
         color="#fdc82c"
       />
       <title>samsisle</title>
@@ -24,7 +24,7 @@ export default function Meta() {
         property="og:description"
         content="Full-stack web developer. SoCal native. Beach lover."
       />
-      <meta property="og:image" content="https://samsisle.dev/static/lib/yellow.png" />
+      <meta property="og:image" content="https://samsisle.dev/static/yellow.png" />
       <meta name="twitter:card" content="summary" />
     </Head>
   );
