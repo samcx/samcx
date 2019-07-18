73a4ff083e31a9d7c602bc62b775a4a3762d553a
samsisle <samko9522@gmail.com>
2019-07-18 16:17:02 -0700

    🍿 Update title and description

--- components/Main.js
+++ components/Main.js
@@ -5,10 +5,10 @@ export default function Main() {
     <main className="m0a pb4 w90 w80-d">
       <section className="pb4">
         <h1 className="fs24 fs36-t fs48-d pb3_2 recoleta">
-          Hi! I'm Sam. Full-stack web developer. SoCal native. Beach lover.
+          Hi! I'm Sam. Full-stack web developer.
         </h1>
-        <p className="black60 fs16 fs20-td lh">
-          Passionate about design, React, and GraphQL.
+        <p className="black60 fs16 fs20-td fsi lh">
+          "People love what other people are passionate about."
         </p>
       </section>
       <Projects />
