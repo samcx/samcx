d68870e112675a4e4c69c2e6e7c6039381bb5fae
samsisle <samko9522@gmail.com>
2019-08-07 12:00:11 -0700

    🎂 Update meta title & meta description

--- components/Main.js
+++ components/Main.js
@@ -5,7 +5,7 @@ export default function Main() {
     <main className="m0a pb4 w90 w80-d">
       <section className="pb4">
         <h1 className="fs24 fs36-t fs48-d pb3_2 recoleta">
-          Hi! I'm Sam. Full-stack web developer.
+          Hi! I'm Sam. Web Engineer.
         </h1>
         <p className="black60 fs16 fs20-td fsi lh">
           "People love what other people are passionate about."
