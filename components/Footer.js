55be82da6df41c9a145cc2a702cd60a25e348e0b
samsisle <samko9522@gmail.com>
2019-08-30 00:44:31 -0700

    🍋 Update classNames

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ import { links } from '../static/links';
 
 export default function Header() {
   return (
-    <footer className="m0a pb4 pt4 w90 w80-d">
+    <footer className="m0a pb40 pt40 w90 w80-d">
       <ul className="flex-m flex-row">
         {links.map(link => (
-          <li className="mr2-d black60 fs16 pb1 pt1 hover" key={link.name}>
+          <li className="mr20-d black60 fs16 pb10 pt10 hover" key={link.name}>
             <a href={link.href} target="_blank" rel="noopener noreferrer">
               {link.name}
             </a>
