17e67b99056b734c093e7ac6a542d39fb6b4ab5e
samsisle <samko9522@gmail.com>
2019-08-30 00:44:53 -0700

    🎂 Update classNames

--- components/Main.js
+++ components/Main.js
@@ -2,9 +2,9 @@ import Projects from './Projects';
 
 export default function Main() {
   return (
-    <main className="m0a pb4 w90 w80-d">
-      <section className="pb4">
-        <h1 className="fs24 fs36-t fs48-d pb3_2 recoleta">
+    <main className="m0a pb40 w90 w80-d">
+      <section className="pb40">
+        <h1 className="fs24 fs36-t fs48-d pb32 recoleta">
           Hi! I'm Sam. Web Engineer.
         </h1>
         <p className="black60 fs16 fs20-td fsi lh">
