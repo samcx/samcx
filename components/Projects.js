dac70422c41f000ddc6e9c1eeb776a7e16b70f81
samsisle <samko9522@gmail.com>
2019-08-30 00:45:04 -0700

    🍬 Update classNames

--- components/Projects.js
+++ components/Projects.js
@@ -2,18 +2,18 @@ import { projects } from '../static/projects';
 
 export default function Projects() {
   return (
-    <section className="pt4">
-      <h2 className="bb bc20 black60 fs16 fs20-td pb2">Projects</h2>
+    <section className="pt40">
+      <h2 className="bb bc20 black60 fs16 fs20-td pb20">Projects</h2>
       <ul className="flex-m flex-d flex-row">
         {projects.map(project => (
-          <li className="bb b-d bc10 pb4 pt4 w30-d" key={project.title}>
+          <li className="bb b-d bc10 pb40 pt40 w30-d" key={project.title}>
             <div className="black60 fs14 fw500 category">{project.category}</div>
             <a href={project.view} target="_blank" rel="noopener noreferrer">
-              <h3 className="fs24 fs36-d fw500 pb1 pt1 recoleta hover">{project.title}</h3>
+              <h3 className="fs24 fs36-d fw500 pb10 pt10 recoleta hover">{project.title}</h3>
             </a>
-            <p className="black60 fs16 lh pb1 pt1">{project.description}</p>
+            <p className="black60 fs16 lh pb10 pt10">{project.description}</p>
             <a href={project.view} target="_blank" rel="noopener noreferrer">
-              <span className="black60 fs14 mr1 hover">View</span>
+              <span className="black60 fs14 mr10 hover">View</span>
             </a>
             <a href={project.source} target="_blank" rel="noopener noreferrer">
               <span className="black60 fs14 hover">Source</span>
