4fa16566778634c5443b4d73e7b666bbfd6049fd
samsisle <samko9522@gmail.com>
2019-09-13 17:27:42 -0700

    🌴 Update next.config.js (withPlugins, etc.)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /**
  * ./next.config.js
- * 
- * See documentation on @zeit/next-css & ZEIT Now below for more information:
+ *
+ * See documentation on next-compose-plugins, next-offline,
+ * @zeit/next-css & ZEIT Now below for more information:
+ *
+ * https://github.com/cyrilwanner/next-compose-plugins
+ * https://github.com/hanford/next-offline#next-export
  * https://github.com/zeit/next-plugins/tree/master/packages/next-css
  * https://zeit.co/docs/v2/deployments/official-builders/next-js-now-next/
  */
 
+const withPlugins = require('next-compose-plugins');
+const withOffline = require('next-offline');
 const withCSS = require('@zeit/next-css');
-module.exports = withCSS({ target: 'serverless' });
+
+const nextConfig = {
+  target: 'severless'
+};
+
+module.exports = withPlugins([withOffline, withCSS], nextConfig);
