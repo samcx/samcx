1568f935d2324d45653ddd047492e5d9646cf2de
samsisle <samko9522@gmail.com>
2019-09-13 18:05:55 -0700

    🌴 Update nextConfig

--- next.config.js
+++ next.config.js
@@ -15,7 +15,27 @@ const withOffline = require('next-offline');
 const withCSS = require('@zeit/next-css');
 
 const nextConfig = {
-  target: 'severless'
+  target: 'serverless',
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    ]
+  }
 };
 
 module.exports = withPlugins([withOffline, withCSS], nextConfig);
