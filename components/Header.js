599d414851adcadb52bc6b357f33fa91617470a7
samsisle <samko9522@gmail.com>
2019-09-27 03:12:02 -0700

    🌮 Add dark-mode toggle

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTransition, animated } from 'react-spring';
+
+import { toggleDarkMode } from '../static/lib/dark-mode';
 
 export default function Header() {
   const [toggle, set] = useState(false);
+
+  useEffect(() => {
+    if (window.localStorage.getItem("dark-mode")) {
+      set(true);
+    }
+  }, [toggle]);
 
   const transitions = useTransition(toggle, null, {
     from: { position: 'absolute', opacity: 0, top: '-100px' },
@@ -11,6 +19,7 @@ export default function Header() {
   });
 
   function handleClick() {
+    toggleDarkMode();
     set(!toggle);
   }
 
