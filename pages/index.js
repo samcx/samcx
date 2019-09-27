206914286aa8862a7e04bbfc8fcdef988b365fca
samsisle <samko9522@gmail.com>
2019-09-27 00:14:07 -0700

    🌲 Move elements over to _app component

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,10 @@
  * on the browser.
  */
 
-import Meta from '../components/Meta';
-import Header from '../components/Header';
 import Main from '../components/Main';
-import Footer from '../components/Footer';
-import Bar from '../components/Bar';
 
 import '../static/css/index.css';
 
 export default function index() {
-  return (
-    <>
-      <Meta />
-      <Header />
-      <Main />
-      <Footer />
-      <Bar />
-    </>
-  );
+  return <Main />;
 }
