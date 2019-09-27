04211a0bd27a0d44b48008fb9a6e82d70c96286e
samsisle <samko9522@gmail.com>
2019-09-27 03:12:59 -0700

    🍬 Add script (check dark-mode on page reload)

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 /**
  * ./pages/_document.js
- * 
+ *
  * _document is only rendered on the server side and not on the client side
  * Event handlers like onClick can't be added to this file
  */
@@ -8,16 +8,17 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html lang="en-US">
         <Head />
         <body>
+          <script
+            /* eslint-disable-next-line react/no-danger */
+            dangerouslySetInnerHTML={{
+              __html: `!function(){window.isDark=!1;try{window.localStorage.getItem("dark-mode")&&(document.documentElement.setAttribute("data-theme","dark"),window.isDark=!0)}catch(t){}}();`
+            }}
+          />
           <Main />
           <NextScript />
         </body>
