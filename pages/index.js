ef3e808980568a0ba43c5816db665498fee9230e
samsisle <samko9522@gmail.com>
2019-10-19 23:50:12 -0700

    🌲 Move index.css to _app.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 /**
- * ./pages/index.js
- *
  * This is the index page. Next.js + Now will automatically process
  * and render this page first upon loading https://samsisle.dev ☀️
  * on the browser.
  */
 
 import Main from '../components/Main';
-
-import '../static/css/index.css';
 
 export default function index() {
   return <Main />;
