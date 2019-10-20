b7bce4332ebf0902ba22ed6e446db2e90b0882cc
samsisle <samko9522@gmail.com>
2019-10-20 01:00:54 -0700

    🎽 Update file paths

--- components/Meta.js
+++ components/Meta.js
@@ -3,18 +3,18 @@ import Head from 'next/head';
 export default function Meta() {
   return (
     <Head>
-      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/static/apple-touch-icon.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="mask-icon"
-        href="/static/safari-pinned-tab.svg"
+        href="/safari-pinned-tab.svg"
         color="#fdc82c"
       />
-      <link rel="manifest" href="/static/manifest.json" />
+      <link rel="manifest" href="/manifest.json" />
       <title>samsisle</title>
       <meta name="theme-color" content="#ffffff" />
       <meta name="description" content="Hi! I'm Sam. Web Engineer." />
@@ -22,7 +22,7 @@ export default function Meta() {
       <meta property="og:description" content="Hi! I'm Sam. Web Engineer." />
       <meta
         property="og:image"
-        content="https://samsisle.dev/static/yellow.png"
+        content="https://samsisle.dev/yellow.png"
       />
       <meta name="twitter:card" content="summary" />
     </Head>
