5cdb496c5fe5526e811a629ff75f350e77714464
samsisle <samko9522@gmail.com>
2019-10-20 01:24:47 -0700

    🌩 Add back styled jsx global styles

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,43 @@ export default class MyApp extends App {
     return (
       <>
         <Meta />
+        <style jsx global>
+          {`
+            @font-face {
+              font-display: swap;
+              font-family: 'Recoleta';
+              font-style: normal;
+              font-weight: 700;
+              src: local('Recoleta Bold'), local('Recoleta-Bold'),
+                url('/Recoleta-Bold.woff2') format('woff2'),
+                url('/Recoleta-Bold.woff') format('woff');
+            }
+            :root {
+              --bg: white;
+              --header-primary: rgba(0, 0, 0, 0.8);
+              --border-primary: rgba(0, 0, 0, 0.2);
+              --border-secondary: rgba(0, 0, 0, 0.1);
+              --text-primary: rgba(0, 0, 0, 0.6);
+              --hover: #fdc82c;
+              --filter: brightness(1) grayscale(0);
+            }
+            [data-theme='dark'] {
+              --bg: rgba(0, 0, 0, 0.8);
+              --header-primary: rgba(255, 255, 255, 0.8);
+              --border-primary: rgba(255, 255, 255, 0.2);
+              --border-secondary: rgba(255, 255, 255, 0.1);
+              --text-primary: rgba(255, 255, 255, 0.6);
+              --hover: #66757f;
+              --filter: brightness(0.75) grayscale(1);
+            }
+            * {
+              box-sizing: border-box;
+            }
+            html {
+              font-size: 62.5%;
+            }
+          `}
+        </style>
         <Header />
         <Component {...pageProps} />
         <Footer />
