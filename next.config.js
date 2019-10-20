75c1cb6e52229413a082eb264f18b669cdd9bb47
samsisle <samko9522@gmail.com>
2019-10-20 00:56:40 -0700

    🌑 Add transformManifest

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const withOffline = require('next-offline');
 const nextConfig = {
   experimental: { css: true },
   target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest),
   workboxOpts: {
     swDest: 'static/service-worker.js',
     runtimeCaching: [
@@ -22,7 +23,7 @@ const nextConfig = {
           networkTimeoutSeconds: 15,
           expiration: {
             maxEntries: 150,
-            maxAgeSeconds: 30 * 24 * 60 * 60
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
           },
           cacheableResponse: {
             statuses: [0, 200]
