38200d55daeb440531943c8c875ff393bfecb496
samsisle <samko9522@gmail.com>
2019-11-15 22:52:23 -0800

    Preload Recoleta-Bold font

--- components/Meta.js
+++ components/Meta.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 export default function Meta() {
   return (
@@ -9,21 +9,21 @@ export default function Meta() {
         sizes="180x180"
         href="/apple-touch-icon.png"
       />
-      <link
-        rel="mask-icon"
-        href="/safari-pinned-tab.svg"
-        color="#fdc82c"
-      />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fdc82c" />
       <link rel="manifest" href="/manifest.json" />
+      <link
+        rel="preload"
+        href="/Recoleta-Bold.woff2"
+        as="font"
+        type="font/woff2"
+        crossOrigin="annonymous"
+      />
       <title>samsisle</title>
       <meta name="theme-color" content="#ffffff" />
       <meta name="description" content="Hi! I'm Sam. Web Engineer." />
       <meta property="og:title" content="samsisle" />
       <meta property="og:description" content="Hi! I'm Sam. Web Engineer." />
-      <meta
-        property="og:image"
-        content="https://samsisle.dev/yellow.png"
-      />
+      <meta property="og:image" content="https://samsisle.dev/yellow.png" />
       <meta name="twitter:card" content="summary" />
     </Head>
   );
