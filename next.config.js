a7fc0125ad3ee2a2ac88178c97f808bdac512bc3
samsisle <samko9522@gmail.com>
2020-01-06 10:54:02 -0800

    Add reactStrictMode config

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const withOffline = require('next-offline');
 
 const nextConfig = {
   experimental: { css: true },
+  reactStrictMode: true,
   target: 'serverless',
   workboxOpts: {
     swDest: 'static/service-worker.js',
