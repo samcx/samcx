9a2ee251e8e9b57a1aca7d11210cc12bcd336168
samsisle <samko9522@gmail.com>
2020-07-03 22:26:36 -0700

    Clear index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,3 @@
-/**
- * This is the index page. Next.js + Now will automatically process
- * and render this page first upon loading https://samsisle.dev ☀️
- * on the browser.
- */
-
-import Main from "../components/Main";
-
 export default function index() {
-  console.log(
-    '%c"People love what other people are passionate about."\n\n — Mia Sebastian, La La Land ☀️',
-    "font-style: italic; font-size: 12px; color: #fdc82c;"
-  );
-  
-  return <Main />;
+  return <div>bruh</div>;
 }
