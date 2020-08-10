a6e0cd45754ba09d59fa6389650a8e6b8122b002
samsisle <samko9522@gmail.com>
2020-08-09 18:38:08 -0700

    Add Retro component + styles

---
+++ components/retro/index.js
@@ -0,0 +1,10 @@
+import styles from './retro.module.css'
+
+export default function Retro() {
+  return (
+    <div className={styles.wrapper}>
+      <p>Sam Ko</p>
+      <p>Web Engineer</p>
+    </div>
+  )
+}
