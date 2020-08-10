9c17a68fcd38b375991d2f19b810c5c62a8459d2
samsisle <samko9522@gmail.com>
2020-08-09 18:38:34 -0700

    Update Index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import Page from '~components/page'
+import Page from '@components/page'
+import Retro from '@components/retro'
 
 export default function Index() {
   return (
     <Page>
-      <div>bruh</div>
+      <Retro />
     </Page>
   )
 }
