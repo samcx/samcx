6c195b8a6c802a21be8f0b6b716b408b12087569
samsisle <samko9522@gmail.com>
2022-01-24 18:35:18 -0800

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import type { NextPage } from 'next'
 
 import Head from 'next/head'
 
-import styles from '@styles/pages/index.module.css'
-
 const Index: NextPage = () => {
   return (
     <>
@@ -14,7 +12,7 @@ const Index: NextPage = () => {
         <meta name="og:description" content="Sam Ko. Web Engineer." />
         <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
       </Head>
-      <main className={styles.wrapper}>
+      <main>
         <img src="/retro.svg" alt="retro" width="300" height="31" />
       </main>
     </>
