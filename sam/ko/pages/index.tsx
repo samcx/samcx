3e88ce24ffec8e876e17aa804bf04225a3464c44
samsisle <samko9522@gmail.com>
2022-07-01 23:46:14 -0700

    Update Index page

--- sam/ko/pages/index.tsx
+++ sam/ko/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 
 import Head from 'next/head'
-import Image from 'next/image'
 
 const Index: NextPage = () => {
   return (
@@ -14,7 +13,7 @@ const Index: NextPage = () => {
         <link rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon" />
       </Head>
       <main>
-        <Image src="/retro.svg" alt="retro" width="300" height="31" />
+        <img src="/retro.svg" alt="retro" width="300" height="31" />
       </main>
     </>
   )
