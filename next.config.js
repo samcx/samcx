47139b0b93b3ffec7996849022647a9b8f00330f
Sam Ko <sam@vercel.com>
2022-10-24 01:11:09 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
+
+module.exports = nextConfig
