195a15718f16270834d2bcfb368d20626ca0d885
Sam Ko <sam@vercel.com>
2023-02-23 19:28:34 -0800

    Update Index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
+import Image from 'next/image'
+import retro from '../public/retro.svg'
+
 export default function Page() {
   return (
     <main>
-      <img src="/retro.svg" alt="retro" width="300" height="31" />
+      <div className="sleeve">
+        <Image
+          alt="logo"
+          height={20}
+          src={retro}
+          style={{ transform: 'rotate(90deg)' }}
+        />
+        <Image
+          alt="logo"
+          height={20}
+          src={retro}
+          style={{ transform: 'rotate(90deg)' }}
+        />
+      </div>
+      <div className="sleeve">
+        <Image
+          alt="logo"
+          height={20}
+          src={retro}
+          style={{ transform: 'rotate(-90deg)' }}
+        />
+        <Image
+          alt="logo"
+          height={20}
+          src={retro}
+          style={{ transform: 'rotate(-90deg)' }}
+        />
+      </div>
     </main>
   )
 }
-
-export const revalidate = 86400;
