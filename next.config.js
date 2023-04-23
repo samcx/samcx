f0963892a24116706ef1dc8bbb50bf2ae102d1c6
Sam Ko <sam@vercel.com>
2023-04-22 21:44:59 -0400

    Switch to temporary redirects

--- next.config.js
+++ next.config.js
@@ -9,17 +9,14 @@ const nextConfig = {
       {
         source: '/issues',
         destination: 'https://github.com/vercel/next.js/issues',
-        permanent: true
       },
       {
         source: '/nr',
         destination: 'https://github.com/vercel/next.js/releases',
-        permanent: true
       },
       {
         source: '/ai',
         destination: 'https://play.vercel.ai/',
-        permanent: true
       }
     ]
   },
