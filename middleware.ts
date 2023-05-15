4c691253ccb3a19f03a907d69821f9e0e070686f
Sam Ko <sam@vercel.com>
2023-05-14 20:26:12 -0700

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -11,8 +11,6 @@ export async function middleware(request: NextRequest) {
   if (edgeConfigKeyExists) {
     const redirect = await get(request.nextUrl.pathname.substring(1)) as string;
     return NextResponse.redirect(redirect);
-  } else {
-    return NextResponse.redirect(new URL('/', request.url));
   }
 }
 
