064f033233cd7f2ba097d40bc8d8679217056833
Sam Ko <sam@vercel.com>
2023-09-02 17:32:38 -0400

    Update imports in Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { get, has } from '@vercel/edge-config';
 
 export async function middleware(request: NextRequest) {
