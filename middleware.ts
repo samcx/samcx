978ab598faea72e5a511cc8b27d743fb77ee1f9b
Sam Ko <sam@vercel.com>
2023-12-17 23:29:09 -0800

    Format middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,22 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { get, has } from '@vercel/edge-config';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { get, has } from '@vercel/edge-config'
 
 export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === '/') {
     return NextResponse.next()
   }
 
-  const edgeConfigKeyExists = await has(request.nextUrl.pathname.substring(1));
+  const edgeConfigKeyExists = await has(request.nextUrl.pathname.substring(1))
 
   if (edgeConfigKeyExists) {
-    const redirect = await get(request.nextUrl.pathname.substring(1)) as string;
-    return NextResponse.redirect(redirect);
+    const redirect = (await get(
+      request.nextUrl.pathname.substring(1),
+    )) as string
+    return NextResponse.redirect(redirect)
   }
 }
 
 export const config = {
   matcher: ['/((?!_next|icon.svg).*)'],
-};
+}
