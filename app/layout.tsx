9381d1309b2c00d35bd4969b09f1e97ab0e4c20d
samcx <sam@vercel.com>
2024-11-28 20:59:01 -0800

    Format Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
