61275f65fb6fd25db23527dfc4de6123ce9dd540
samcx <sam@vercel.com>
2024-12-25 18:15:20 -0800

    Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ export default function Home() {
     <main>
       <div>
         <p className="text">
-          Eat at a local resturant tonight. Get the cream sauce. Have a cold pint
-          at 4 o&apos;clock in a mostly empty bar. Go somewhere you&apos;ve never
-          been. Listen to someone you think may have nothing in common with you.
-          Order the steak rare. Eat an oyster. Be open to a world where you may
-          not understand or agree with the person next to you, but have a drink
-          with them anyways. Eat slowly. Tip your server. Check in on your [family
-          and] and friends. Check in on yourself. Enjoy the ride.
+          Eat at a local resturant tonight. Get the cream sauce. Have a cold
+          pint at 4 o&apos;clock in a mostly empty bar. Go somewhere you&apos;ve
+          never been. Listen to someone you think may have nothing in common
+          with you. Order the steak rare. Eat an oyster. Be open to a world
+          where you may not understand or agree with the person next to you, but
+          have a drink with them anyways. Eat slowly. Tip your server. Check in
+          on your [family and] and friends. Check in on yourself. Enjoy the
+          ride.
         </p>
       </div>
     </main>
