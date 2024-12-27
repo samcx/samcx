0c721b48e515d596dd11c2c3c0e045b42e607432
samcx <sam@vercel.com>
2024-12-27 00:23:31 -0800

    Add Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,9 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer>
-      <Link href="https://x.com/samko_cx/" target="_blank">𝕏</Link>
+      <Link href="https://x.com/samko_cx/" target="_blank">
+        𝕏
+      </Link>
     </footer>
   )
 }
