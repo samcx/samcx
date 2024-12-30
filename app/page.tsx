a182c70b19463f0f76675a5035c42b6a4e6902a4
samcx <sam@vercel.com>
2024-12-29 17:44:59 -0800

    Update serif + italics balance

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@ export default function Home() {
     <main>
       <div>
         <p className="quote">
-          Eat at a local resturant tonight. Get the cream sauce. Have a cold
-          pint at 4 o&apos;clock in a mostly empty bar.{' '}
+          <em>Eat at a local resturant tonight.</em> Get the cream sauce. Have a
+          cold pint at 4 o&apos;clock in a mostly empty bar.{' '}
           <em>Go somewhere you&apos;ve never been.</em> Listen to someone you
           think may have nothing in common with you. Order the steak rare. Eat
           an oyster. <em>Be open to a world</em> where you may not understand or
-          agree with the person next to you, but have a drink with them anyways.
-          Eat slowly. Tip your server. Check in on your [family and] and
-          friends. Check in on yourself. <em>Enjoy the ride.</em>
+          agree with the person next to you,{' '}
+          <em>but have a drink with them anyways.</em> Eat slowly. Tip your
+          server. <em>Check in</em> on your [family and] and friends. Check in
+          on yourself. <em>Enjoy the ride.</em>
         </p>
       </div>
     </main>
