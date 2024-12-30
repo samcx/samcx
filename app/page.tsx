8be4ac1153014c95f37a0c49884ad971e22948b2
samcx <sam@vercel.com>
2024-12-29 17:30:40 -0800

    Update san serif and serif combination

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,15 @@ export default function Home() {
   return (
     <main>
       <div>
-        <p className="text">
+        <p className="quote">
           Eat at a local resturant tonight. Get the cream sauce. Have a cold
-          pint at 4 o&apos;clock in a mostly empty bar. Go somewhere you&apos;ve
-          never been. Listen to someone you think may have nothing in common
-          with you. Order the steak rare. Eat an oyster. Be open to a world
-          where you may not understand or agree with the person next to you, but
-          have a drink with them anyways. Eat slowly. Tip your server. Check in
-          on your [family and] and friends. Check in on yourself. Enjoy the
-          ride.
+          pint at 4 o&apos;clock in a mostly empty bar.{' '}
+          <em>Go somewhere you&apos;ve never been.</em> Listen to someone you
+          think may have nothing in common with you. Order the steak rare. Eat
+          an oyster. <em>Be open to a world</em> where you may not understand or
+          agree with the person next to you, but have a drink with them anyways.
+          Eat slowly. Tip your server. Check in on your [family and] and
+          friends. Check in on yourself. <em>Enjoy the ride.</em>
         </p>
       </div>
     </main>
