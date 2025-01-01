120cd4ddc65042be423cd6f470d965e2c57bf2df
samcx <sam@vercel.com>
2024-12-31 18:23:03 -0800

    Add Insta link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Instagram from '@/public/instagram.svg'
 
 export default function Footer() {
   return (
     <footer>
       <Link href="https://x.com/samko_cx/" target="_blank">
         𝕏
+      </Link>
+      <Link href="https://www.instagram.com/samko_cx/" target="_blank">
+        <Image src={Instagram} alt="Instagram icon" height={20} width={20} unoptimized />
       </Link>
     </footer>
   )
