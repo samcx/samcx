67a080b0a8da975f72002c1a6ed305be03c539e2
samcx <sam@vercel.com>
2025-01-24 22:04:54 -0800

    Add GitHub icon/link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import GitHub from '@/public/github.svg'
 import Instagram from '@/public/instagram.svg'
 
 export default function Footer() {
   return (
     <footer>
+      <Link href="https://github.com/samcx/" target="_blank">
+        <Image
+          src={GitHub}
+          alt="GitHub icon"
+          height={12}
+          width={17}
+          unoptimized
+        />
+      </Link>
       <Link href="https://x.com/samko_cx/" target="_blank">
         𝕏
       </Link>
