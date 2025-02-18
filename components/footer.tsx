3bf950e66fafc893f858701f9948cfbe65c5e6b7
samcx <sam@vercel.com>
2025-02-17 21:23:54 -0800

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,36 +8,38 @@ import Instagram from '@/public/instagram.svg'
 export default function Footer() {
   return (
     <footer>
-      <Link href="https://github.com/samcx/" target="_blank">
-        <Image
-          src={GitHub}
-          alt="GitHub icon"
-          height={12}
-          width={17}
-          unoptimized
-        />
-      </Link>
-      <Link href="https://x.com/samko_cx/" target="_blank">
-        <Image src={X} alt="X icon" height={18} width={18} unoptimized />
-      </Link>
-      <Link href="https://www.linkedin.com/in/samcx/" target="_blank">
-        <Image
-          src={LinkedIn}
-          alt="LinkedIn icon"
-          height={13}
-          width={20}
-          unoptimized
-        />
-      </Link>
-      <Link href="https://www.instagram.com/samko_cx/" target="_blank">
-        <Image
-          src={Instagram}
-          alt="Instagram icon"
-          height={20}
-          width={20}
-          unoptimized
-        />
-      </Link>
+      <div>
+        <Link href="https://github.com/samcx/" target="_blank">
+          <Image
+            src={GitHub}
+            alt="GitHub icon"
+            height={12}
+            width={17}
+            unoptimized
+          />
+        </Link>
+        <Link href="https://x.com/samko_cx/" target="_blank">
+          <Image src={X} alt="X icon" height={18} width={18} unoptimized />
+        </Link>
+        <Link href="https://www.linkedin.com/in/samcx/" target="_blank">
+          <Image
+            src={LinkedIn}
+            alt="LinkedIn icon"
+            height={13}
+            width={20}
+            unoptimized
+          />
+        </Link>
+        <Link href="https://www.instagram.com/samko_cx/" target="_blank">
+          <Image
+            src={Instagram}
+            alt="Instagram icon"
+            height={20}
+            width={20}
+            unoptimized
+          />
+        </Link>
+      </div>
     </footer>
   )
 }
