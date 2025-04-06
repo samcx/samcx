c6b79e70a82fe3b939d255d445b35ca469938b97
samcx <sam@vercel.com>
2025-04-05 23:13:34 -0700

    Add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <p className="quote">
+        <em>
+          There is nothing to see here. Well, other than these two sentences...
+        </em>
+      </p>
+    </main>
+  )
+}
