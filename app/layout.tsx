35ae8eee2f35444b29e928f40b111b4292c56a4d
samcx <sam@vercel.com>
2025-04-05 23:13:41 -0700

    Refactor <main>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
-import Footer from '@/components/footer'
 import './globals.css'
 
 const geistSans = Geist({
@@ -21,7 +20,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={geistSans.className}>
         {children}
-        <Footer />
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
+import Footer from '@/components/footer'
+
 export default function Home() {
   return (
-    <main>
-      <div>
-        <p className="quote">
-          <em>Eat at a local resturant tonight.</em> Get the cream sauce. Have a
-          cold pint at 4 o&apos;clock in a mostly empty bar.{' '}
-          <em>Go somewhere you&apos;ve never been.</em> Listen to someone you
-          think may have nothing in common with you. Order the steak rare. Eat
-          an oyster. <em>Be open to a world</em> where you may not understand or
-          agree with the person next to you,{' '}
-          <em>but have a drink with them anyways.</em> Eat slowly. Tip your
-          server. Check in on your [family and] friends. Check in on yourself.{' '}
-          <em>Enjoy the ride.</em>
-        </p>
-      </div>
-    </main>
+    <>
+      <main className="page">
+        <div>
+          <p className="quote">
+            <em>Eat at a local resturant tonight.</em> Get the cream sauce. Have
+            a cold pint at 4 o&apos;clock in a mostly empty bar.{' '}
+            <em>Go somewhere you&apos;ve never been.</em> Listen to someone you
+            think may have nothing in common with you. Order the steak rare. Eat
+            an oyster. <em>Be open to a world</em> where you may not understand
+            or agree with the person next to you,{' '}
+            <em>but have a drink with them anyways.</em> Eat slowly. Tip your
+            server. Check in on your [family and] friends. Check in on yourself.{' '}
+            <em>Enjoy the ride.</em>
+          </p>
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
